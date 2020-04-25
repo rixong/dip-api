79e48860aa4160f6f84b3f6be89f2d2337ad794b
rixong <rixong@gmail.com>
2020-04-25 15:58:44 -0500

    new seeds

--- app/serializers/reservation_serializer.rb
+++ app/serializers/reservation_serializer.rb
@@ -1,11 +1,19 @@
 class ReservationSerializer < ActiveModel::Serializer
-  attributes :id, :departure, :arrival, :reserver
+  
+  attributes :id, :departure, :arrival, :reserver, :cabin
 
   def reserver
     {firstname: self.object.user.firstname,
       lastname: self.object.user.lastname,
       email: self.object.user.email,
       # reserverName: `#{self.object.user.firstname} #{self.object.user.lastname}`
+    }
+  end
+
+  def cabin
+    {cabinName: self.object.cabin.name,
+      cabinId: self.object.cabin.id,
+      cabinMultiplier: self.object.cabin.multiplier,
     }
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,14 +13,20 @@ User.create(email: 'bob@aol.com', password: '1234', password_confirmation: '1234
 User.create(email: 'con@aol.com', password: '1234', password_confirmation: '1234', firstname: 'Connie', lastname: 'Crawford', bday: 20010602)
 
 Cabin.create(name: 'Big House', description: 'The grandest and oldest', multiplier: 1)
-Cabin.create(name: 'Pine Away', description: 'The second to be built. Sewells home originally.', multiplier: 0.75)
+Cabin.create(name: 'Pine Away', description: 'Down by the waters edge.', multiplier: 0.75)
 Cabin.create(name: 'Winter Haven', description: 'Ken and Betty Emersons home built in the 70s. Two bedroom.', multiplier: 0.5)
+Cabin.create(name: 'Gray House', description: 'The second to be built. Sewells home originally.', multiplier: 0.5)
+Cabin.create(name: 'Hillside', description: 'Small, cozy accomodations near at the bottom of the hill. Used by Josephine Emerson in her later years.', multiplier: 0.5)
+Cabin.create(name: 'Brownie Cottage', description: 'Feel like your right on the water. Rustic but cozy.', multiplier: 0.25)
+Cabin.create(name: 'Guest House', description: 'Frequent hangout for the younger, single set', multiplier: 0.25)
 
 Repair.create(category: 'plumbing', description: 'Leaky faucet', submission_date: 20200601, priority: true, followup: 'none', user_id: 1, cabin_id: 1, annual_report_id: 1)
 Repair.create(category: 'plumbing', description: 'No hot water', submission_date: 20200621, priority: false, followup: 'none', user_id: 1, cabin_id: 2, annual_report_id: 1)
 Repair.create(category: 'electrical', description: 'Kitchen light shorted out.', submission_date: 20200711, priority: true, followup: 'none', user_id: 2, cabin_id: 1, annual_report_id: 1)
 Repair.create(category: 'capentry', description: 'Deck is rotting out', submission_date: 20200803, priority: false, followup: 'none', user_id: 3, cabin_id: 1, annual_report_id: 1)
 
-Reservation.create(arrival: 20200620, departure: 20200624, user_id: 1, cabin_id: 1, annual_report_id: 1)
+Reservation.create(arrival: 20200531, departure: 20200602, user_id: 1, cabin_id: 1, annual_report_id: 1)
+Reservation.create(arrival: 20200604, departure: 20200610, user_id: 2, cabin_id: 1, annual_report_id: 1)
 Reservation.create(arrival: 20200605, departure: 20200610, user_id: 2, cabin_id: 2, annual_report_id: 1)
-Reservation.create(arrival: 20200610, departure: 20200620, user_id: 3, cabin_id: 3, annual_report_id: 1)
+Reservation.create(arrival: 20200601, departure: 20200620, user_id: 3, cabin_id: 3, annual_report_id: 1)
+Reservation.create(arrival: 20200601, departure: 20200603, user_id: 3, cabin_id: 5, annual_report_id: 1)
