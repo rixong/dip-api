977cbd4521dda010d438a943a2ae27e5f7a661c5
rixong <rixong@gmail.com>
2020-05-10 07:40:10 -0500

    simplify serializers

--- app/controllers/api/v1/annual_reports_controller.rb
+++ app/controllers/api/v1/annual_reports_controller.rb
@@ -2,15 +2,7 @@ class Api::V1::AnnualReportsController < ApplicationController
 
   def current
     report = AnnualReport.find_by(current: true)
-    users = User.all
-    cabins = Cabin.all
-    reservations = Reservation.all
-    repairs = Repair.all
-    render json: {report: report, 
-                  users: users, 
-                  cabins: cabins, 
-                  reservations: reservations,
-                  repairs: repairs}
+    render json: {report: AnnualReportSerializer.new(report), message: 'success'}
   end
   
 
